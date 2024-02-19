4b0109680e77f1e22b29dc8f1d4ca91c208f31fc
Santiago E <santiago93echevarria@gmail.com>
2024-02-19 15:53:39 -0300

    Update core.c

--- src/core.c
+++ src/core.c
@@ -7,6 +7,7 @@
 
 #define PRINTFC(c,t,f,s) printf("\033[%d;%dm" f "\033[0m",t ,28 + c, s)
 #define PUTSC(c,f)     printf("\033[%dm" f  "\033[0m", 30 + c)
+
 #define C_RED     1
 #define C_GREEN   2
 #define C_YELLOW  3
@@ -21,12 +22,30 @@ void main()
     int i, j, k;
     int dim, nbombas, nvisibles, nlibres;
     int seguir = 1, fi, co;
-    int A[100][100], VISIBLE[100][100];
-    printf("\n\n\tDimension: ");
-    dim = 20;//getchar();
-    printf("\n\tCantidad de bombas:");
-    nbombas = 10;//getchar();
+
+    // printf("\n\n\tDimension: ");
+    // scanf("%d", &dim);
+    // printf("\n\tCantidad de bombas:");
+    // scanf("%d", &nbombas);
+
+    dim=20;
+    nbombas=100;
+
+    int A[dim+1][dim+1];
+    int VISIBLE[dim+1][dim+1];
+    int a=0;
+
     nlibres = dim * dim - nbombas; // Cantidad de celdas que no son bombas
+
+      for (i = 0; i <= dim; i++)
+    {
+        for (j = 0; j <= dim; j++)
+        {
+            VISIBLE[i][j] = 0;
+            A[i][j] = 0;
+        };
+    };
+
 
     // Ubicar las bombas en posiciones aleatorias
     for (i = 1; i <= nbombas; i++)
@@ -42,21 +61,25 @@ void main()
 
     // The cell value is the amount of bombs around it
     // Set non-bomb cell values
+    a=0;
     for (i = 1; i <= dim; i++)
     {
         for (j = 1; j <= dim; j++)
         {
-            if (A[i][j] != 9)
+            VISIBLE[i][j] = 0;
+            if (A[i][j] != BOMB)
             {
-                int a;
-                a = a + div(A[i - 1][j - 1], BOMB).quot; // up-left
-                a = a + div(A[i - 1][j], BOMB).quot;     // up
-                a = a + div(A[i - 1][j + 1], BOMB).quot; //up-right
-                a = a + div(A[i][j - 1], BOMB).quot;     //left
-                a = a + div(A[i][j + 1], BOMB).quot;     //right
-                a = a + div(A[i + 1][j - 1], BOMB).quot; // down-left
-                a = a + div(A[i + 1][j], BOMB).quot;     //down
-                a = a + div(A[i + 1][j + 1], BOMB).quot; //down-right
+                a=0;
+                a = a + (int)( A[i - 1][j - 1] == BOMB ); // up-left
+                a = a + (int)( A[i - 1][j] == BOMB );     // up
+                a = a + (int)( A[i - 1][j + 1] == BOMB ); //up-right
+                a = a + (int)( A[i][j - 1] == BOMB );     //left
+                a = a + (int)( A[i][j + 1] == BOMB );     //right
+                a = a + (int)( A[i + 1][j - 1] == BOMB ); // down-left
+                a = a + (int)( A[i + 1][j] == BOMB );     //down
+                a = a + (int)( A[i + 1][j + 1] == BOMB ); //down-right
+
+                A[i][j] = (int)a;
             };
         };
     };
@@ -66,22 +89,25 @@ void main()
 
         // Actualizar pantalla
         system("clear");
-        printf("%c x %c. \t\tFaltan %c / %c \n\n\n\n\n\t","",dim,dim,nlibres-nvisibles,nlibres);
+        printf("%d x %d. \t\tFaltan %d / %d \n\n\n\n\n\t",dim,dim,nlibres-nvisibles,nlibres);
         k = (dim - (dim % 10)) / 10;
         for (i = 1; i <= k; i++)
         {
-            printf("                   %c",i);
+            printf("                   %d",i);
         };
         printf("\n\t");
         // Imprimir mapa
         for (i = 1; i <= dim; i++)
         {
-            printf(" %c", i % 10);
+            printf(" %d", i % 10);
         };
         printf("\n");
+
+        // Imprimir mapa
         for (i = 1; i <= dim; i++)
+         for (j = 1; j <= dim; j++){
         {
-            printf("%c\t",i);
+            printf("%d\t",i);
             for (j = 1; j <= dim; j++)
             {
                 printf(" ");
@@ -89,81 +115,110 @@ void main()
                 {
                     if (A[i][j] == 9)
                     {
-                        printf("*");
+
+                        PUTSC(C_RED,"*");
                     }
                     else if (A[i][j] == 0)
                     {
+                     
+                     
                         printf(" ");
                     }
                     else
                     {
-                        printf("%c",A[i][j]);
+//                        printf("%d",A[i][j]);
+
+                        printf("\033[%dm" "%d" "\033[0m",30 + A[i][j], A[i][j]);
+                     
                     };
                 }
                 else
                 {
-                    printf("#");
+                     printf("#");
+                     //printf("%d",A[i][j]);
+                    // PUTSC(C_BLUE,"#");
                 };
             };
             printf("\n");
         };
+         };
+        
+        
+        //  elegir coordenadas
         printf("\n\nFila ");
-        fi = getchar();
+        scanf("%d", &fi);
         printf("Columna ");
-        co = getchar();
+        scanf("%d", &co);
+
+        // Hacer visible la elegida
         VISIBLE[fi][co] = 1;
+
+        // Si es bomba, perder
         if (A[fi][co] == 9)
         {
             system("clear");
             printf("\n\t\tPERDISTE\n\n");
             seguir = 0;
         };
+
+
+        // Hacer visibles vecinas
         for (k = 0; k <= nlibres; k++)
         {
-            for (i = 1; i <= dim; i++)
-            {
+            for (i = 1; i <= dim; i++){
                 for (j = 1; j <= dim; j++)
                 {
-                    if (VISIBLE[i - 1][j - 1] == 1 && A[i - 1][j - 1] == 0)
-                    {
-                        VISIBLE[i][j] = 1;
-                    };
+
+                    // top-left
+                    // if (VISIBLE[i - 1][j - 1] == 1 && A[i - 1][j - 1] == 0)
+                    // {
+                    //     VISIBLE[i][j] = 1;
+                    // };
+                    // top-center
                     if (VISIBLE[i - 1][j] == 1 && A[i - 1][j] == 0)
                     {
                         VISIBLE[i][j] = 1;
                     };
-                    if (VISIBLE[i - 1][j + 1] == 1 && A[i - 1][j + 1] == 0)
-                    {
-                        VISIBLE[i][j] = 1;
-                    };
+                    //top-right
+                    // if (VISIBLE[i - 1][j + 1] == 1 && A[i - 1][j + 1] == 0)
+                    // {
+                    //     VISIBLE[i][j] = 1;
+                    // };
+                    // center-left
                     if (VISIBLE[i][j - 1] == 1 && A[i][j - 1] == 0)
                     {
                         VISIBLE[i][j] = 1;
                     };
+                    // center-right
                     if (VISIBLE[i][j + 1] == 1 && A[i][j + 1] == 0)
                     {
                         VISIBLE[i][j] = 1;
                     };
+                    // bottom-left
                     if (VISIBLE[i + 1][j - 1] == 1 && A[i + 1][j - 1] == 0)
                     {
                         VISIBLE[i][j] = 1;
                     };
+                    // bottom-center
                     if (VISIBLE[i + 1][j] == 1 && A[i + 1][j] == 0)
                     {
                         VISIBLE[i][j] = 1;
                     };
+                    // bottom-right
                     if (VISIBLE[i + 1][j + 1] == 1 && A[i + 1][j + 1] == 0)
                     {
                         VISIBLE[i][j] = 1;
+                        };
                     };
                 };
             };
-        };
         nvisibles = 0;
+
         for (i = 1; i <= dim; i++)
         {
             for (j = 1; j <= dim; j++)
             {
+                //
                 if (VISIBLE[i][j] == 1)
                 {
                     nvisibles++;
@@ -189,7 +244,7 @@ void main()
                     }
                     else
                     {
-                        printf("%c",A[i][j]);
+                        printf("%d",A[i][j]);
                     };
                     printf(" ");
                 };
